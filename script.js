2b1a2a53d1e5102945044fd1c6bb71228baf311a
Laura Graham <lauragra@microsoft.com>
2015-08-28 10:18:35 -0700

    Added sample files.

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// The initialize function must be run each time a new page is loaded
+Office.initialize = function (reason) {
+	$(document).ready(function () {
+		$('#set-data').click(writeText);
+	});
+
+	 //UI Components init
+     $(".ms-Pivot").Pivot();
+     $(".ms-SearchBox").SearchBox();
+     $(".ms-Dropdown").Dropdown();
+};
+
+// Reads data from current document selection and displays a notification
+function writeText() {
+    Office.context.document.setSelectedDataAsync("Citation goes here",
+        function (asyncResult) {
+            var error = asyncResult.error;
+            if (asyncResult.status === "failed"){
+            	$('#display-data').text("Failure" + error.message);
+            }
+            else
+            {
+            	$('#display-data').text("Done");
+            }
+        });
+}
